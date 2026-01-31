8e3ce662cb90fc22ffbc4e33ed2e41e31000852c
Yaw <yfrimpong@gmail.com>
2026-01-31 15:19:26 -0600

    Updated deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e. # Exit immediately if a command fails
+set -e # Exit immediately if a command fails
 
 #Variables
 AWS_REGION="us-east-1"
 ACCOUNT_ID="336107977801"
 IMAGE_NAME="bigdata-job"
-IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}://{IMAGE_NAME}:latest"
+IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}:latest"
+// 336107977801.dkr.ecr.us-east-1.amazonaws.com/bigdata-job:latest
+
 TAG=$(git rev-parse --short HEAD)
 
 echo "Logging into ECR..."
